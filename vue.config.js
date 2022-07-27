c4fc4fd68fcbbfe2e8098b50b9d382a1b71f7d4c
慕师 <xunisanshipingan@foxmail.com>
2022-07-27 15:19:25 +0800

    publicPath fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path') 
 module.exports = {
-    publicPath: '/Blog/',
+    publicPath: './',
     pluginOptions :{
         'style-resources-loader': {
             preProcessor: 'less',
