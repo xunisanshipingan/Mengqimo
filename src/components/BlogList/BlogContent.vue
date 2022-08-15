f014e4038b906b8f22aa1e5a548f05d02ddd65bc
慕师 <xunisanshipingan@foxmail.com>
2022-08-15 16:50:19 +0800

    blogList-page √

---
+++ src/components/BlogList/BlogContent.vue
@@ -0,0 +1,50 @@
+<!-- blogs(获取列表并显示) -->
+<template>
+    <a href="" class="bloginfo">
+        <span class="title">{{title}}</span>
+        <span class="content">{{content}}</span>
+        <span class="timedate">{{timedate}}</span>
+    </a>
+</template>
+
+<script setup>
+const { ref }=require("@vue/reactivity")
+
+    const props = defineProps({
+        title : {type:[Number,String], default: "标题"},
+        content: {type:[Number, String], default: "内容"},
+        timedate: {type:[Number, String], default: "1970-01-01 00:00:00"}
+    })
+    const title = ref(props.title),
+          content = ref(props.content),
+          timedate = ref(props.timedate)
+</script>
+
+<style lang="less" scoped>
+    .bloginfo{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 25px;
+        .title{
+            font-weight: bold;
+            font-size: 16px;
+            margin: 10px 0;
+        }
+        .content{
+            .FontStyle();
+            font-size: 16px;
+            text-align: start;
+            margin: 10px 0;
+            overflow: hidden;
+            display: block;
+            white-space: nowrap;
+            width: -webkit-fill-available;
+        }
+        .timedate{
+            .FontStyle();
+            opacity: 0.6;
+            font-size: 11px;
+        }
+    }
+</style>
